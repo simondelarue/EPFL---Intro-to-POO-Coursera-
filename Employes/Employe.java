902de332e5605a31f7a67a242171f551576dc1bb
simondelarue <sim.delarue@gmail.com>
2020-07-16 13:52:11 +0200

    Add files via upload

---
+++ Employes/Employe.java
@@ -0,0 +1,21 @@
+package employes;
+
+abstract class Employe {
+
+	private String nom;
+	
+	public Employe(String unNom){
+		nom = unNom;
+	}
+	
+	public String getNom(){
+		return this.nom;
+	}
+	
+	abstract public double salaireCumule();
+	
+	public void affiche(){
+		System.out.println(" nom de l'employe : " + this.getNom());
+		System.out.println(" salaire cumule : " + salaireCumule());
+	}
+}
---
+++ Employes/Vendeur.java
@@ -0,0 +1,39 @@
+package employes;
+
+public class Vendeur extends EmployeTemporaire {
+	
+	private double volumeVente;
+	private double commission;
+
+	// Constructeur de base
+	public Vendeur(String unNom, double unSalaireHor, double unNbHeuresMois, double unVolumeVente, double uneCommission) {
+		super(unNom, unSalaireHor, unNbHeuresMois);
+		volumeVente = unVolumeVente;
+		commission = uneCommission;
+	}
+	// Constructeur pour la mutation
+	public Vendeur(Employe ancien, double unSalaireHor, double unNbHeuresMois, double uneCommission) {
+		this(ancien.getNom(), unSalaireHor, unNbHeuresMois, 0.0, uneCommission);
+		volumeVente = (double)(ancien.salaireCumule() / getCommission());
+	}
+	
+	public double getVolumeVente(){
+		return volumeVente;
+	}
+	public double getCommission(){
+		return commission;
+	}
+	
+	private double getMtCommission(){
+		return getVolumeVente() * getCommission();
+	}
+	@Override
+	public double salaireCumule() {
+		return super.salaireCumule() + getMtCommission() ;
+	}
+	public void affiche(){
+		super.affiche();
+		System.out.println("Vendeur");
+	}
+
+}
